e863c29e5fde3b6b6ed20862261091a02ef04a24
Mark Ahrens <ahrens.mark.a@gmail.com>
2019-08-14 10:32:23 -0500

    annotations exercise

--- src/Author.java
+++ src/Author.java
@@ -1,6 +1,10 @@
+
+
 import java.util.ArrayList;
 import java.util.List;
+
 public class Author extends Person {
+    @SuppressWarnings("rawtypes")
     private List books;
 
     public Author(String firstName, String lastName) {
@@ -9,10 +13,11 @@ public class Author extends Person {
     }
 
     /**
-     * @deprecated Use addBook instead
+     * @deprecated Use publishedBooks instead
      */
-    @Deprecated
-    @SuppressWarnings("unchecked")
+
+
+    @SuppressWarnings("deprecation")
     public List<String> getBooks() {
         return books;
     }
@@ -28,48 +33,8 @@ public class Author extends Person {
     }
 
     @Override
-    //public String sortName() {
-        public String fullName () {
-            return String.format("%s, %s", lastName, firstName);
-        }
+    public String fullName()
+    {
+        return String.format("%s, %s", lastName, firstName);
     }
-
-
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class Author extends Person {
-//    @SuppressWarnings("rawtypes")
-//    private List books;
-//
-//    public Author(String firstName, String lastName) {
-//        super(firstName, lastName);
-//        books = new ArrayList();
-//    }
-//
-//    /**
-//     * @deprecated Use publishedBooks instead
-//     */
-//
-//
-//    @SuppressWarnings("deprecation")
-//    public List<String> getBooks() {
-//        return books;
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    public List<String> publishedBooks() {
-//        return books;
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    public void addBook(String book) {
-//        books.add(book);
-//    }
-//
-//    @Override
-//    public String fullName()
-//    {
-//        return String.format("%s, %s", lastName, firstName);
-//    }
-//}
+}
